08f11980495de948054a0e89b93a87ebe492a6b5
Fillip Grønkjær Jensen <fill0246@student.eadania.dk>
2020-01-15 13:17:36 +0100

    Add project files.

---
+++ HovedmenuMedlem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfBklApp
+{
+    /// <summary>
+    /// Interaction logic for HovedmenuMedlem.xaml
+    /// </summary>
+    public partial class HovedmenuMedlem : Window
+    {
+        public HovedmenuMedlem()
+        {
+            InitializeComponent();
+            //Test
+        }
+    }
+}
